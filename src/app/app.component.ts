c32ca4c8a1703b358f3ad1d429f684bd10f68a4c
jmusau62-gmail <jmusau62@gmail.com>
2021-07-18 20:38:55 +0300

    added to quotes array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
-
+import {Quote} from './quote';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes: string [];
-  constructor (){
-    this.quotes = ['Wooo','Bullet','Games']
-  }
+  quotes:Quote[] = [ 
+    {id:1, name:'Wooo'}  ,
+     {id:2, name:'Bullets'} ,
+   { id:3 ,name:'Games'},
+   {id:4 ,name:'Greatness is here'},
+   {id:5 ,name: 'Do your math' },
+   {id:6,name:'Plot my world domination plan'}
+  ];
 }
